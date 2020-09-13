a802e8b9c9471682b888bd51b54dae8cf3707f6a
xiaoyin366 <601523360@qq.com>
2020-09-13 23:21:29 +0800

    doc：提交controller，service,dao结构代码

--- src/main/java/com/springcolud/controller/HelloController.java
+++ src/main/java/com/springcolud/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.springcolud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class HelloController {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(HelloController.class);
 
-        @RequestMapping("/hello")
-        public String hello(){
-         return "this is my first project by springboot";
-        }
+    @RequestMapping("/hello")
+     public String hello(){
+        log.info("21321321321");
+       return "this is my first project by springboot";
+     }
 }
